cd60c808e50f8460992d1dbd23faa583077d0c1c
cristhianchimbo50 <chimbocristhian994@gmail.com>
2024-01-25 17:37:39 -0500

    otras modificaciones

--- Proyecto V1.py
+++ Proyecto V1.py
@@ -1,16 +1,16 @@
 # Inicio de Sesión
 print("Bienvenido Usuario")
 
-# Inicialización de productos y existencias
-producto1, existencia1 = "Producto1", 0
-producto2, existencia2 = "Producto2", 0
-producto3, existencia3 = "Producto3", 0
+# Inicialización de productos y cantidades
+Arroz, cantidadArroz = "Arroz", 0
+Cola, cantidadCola = "Cola", 0
+Helado, cantidadHelado = "Helado", 0
 
 while True:
     usuario = input("Ingrese su nombre de usuario: ")
     contrasena = input("Ingrese su contraseña: ")
 
-    if usuario == 'usuario1' and contrasena == 'contrasena1':
+    if usuario == 'sebas' and contrasena == '12345':
         print("Inicio de sesión exitoso.")
         break
     else:
@@ -27,41 +27,44 @@ while True:
     opcion = input("Seleccione una opción (1-5): ")
 
     if opcion == '1':
+        print("Agregar Producto...\n")
         nombre_producto = input("Ingrese el nombre del nuevo producto: ")
         existencias = int(input("Ingrese las existencias iniciales: "))
-        if nombre_producto == "Producto1":
-            existencia1 += existencias
-        elif nombre_producto == "Producto2":
-            existencia2 += existencias
-        elif nombre_producto == "Producto3":
-            existencia3 += existencias
+        if nombre_producto == "Arroz":
+            cantidadArroz += existencias
+        elif nombre_producto == "Cola":
+            cantidadCola += existencias
+        elif nombre_producto == "Helado":
+            cantidadHelado += existencias
         else:
             print("Producto no válido. Inténtelo nuevamente.")
 
         print("Producto agregado con éxito.")
 
     elif opcion == '2':
+        print("Actualizar Existencia...\n")
         nombre_producto = input("Ingrese el nombre del producto a actualizar: ")
         existencias = int(input("Ingrese las nuevas existencias: "))
-        if nombre_producto == "Producto1":
-            existencia1 = existencias
-        elif nombre_producto == "Producto2":
-            existencia2 = existencias
-        elif nombre_producto == "Producto3":
-            existencia3 = existencias
+        if nombre_producto == "Arroz":
+            cantidadArroz = existencias
+        elif nombre_producto == "Cola":
+            cantidadCola = existencias
+        elif nombre_producto == "Helado":
+            cantidadHelado = existencias
         else:
             print("Producto no encontrado. Inténtelo nuevamente.")
 
         print("Existencias actualizadas con éxito.")
 
     elif opcion == '3':
+        print("Eliminar Producto...\n")
         nombre_producto = input("Ingrese el nombre del producto a eliminar: ")
-        if nombre_producto == "Producto1":
-            existencia1 = 0
-        elif nombre_producto == "Producto2":
-            existencia2 = 0
-        elif nombre_producto == "Producto3":
-            existencia3 = 0
+        if nombre_producto == "Arroz":
+            cantidadArroz = 0
+        elif nombre_producto == "Cola":
+            cantidadCola = 0
+        elif nombre_producto == "Helado":
+            cantidadHelado = 0
         else:
             print("Producto no encontrado. Inténtelo nuevamente.")
 
@@ -69,12 +72,12 @@ while True:
 
     elif opcion == '4':
         print("\nInventario:")
-        print(f"{producto1}: {existencia1} unidades")
-        print(f"{producto2}: {existencia2} unidades")
-        print(f"{producto3}: {existencia3} unidades")
+        print(f"{Arroz}: {cantidadArroz} unidades")
+        print(f"{Cola}: {cantidadCola} unidades")
+        print(f"{Helado}: {cantidadHelado} unidades")
 
     elif opcion == '5':
-        print("Saliendo del programa. ¡Hasta luego!")
+        print("chao")
         break
 
     else:
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ProyectoSupermercado
 Desarrollo de un Menú de Gestión de Supermercado
+
+El programa es un sistema de autenticación y gestión de inventario en Python. 
+
+Después de la autenticación del usuario ofrece un menú para agregar productos con cantidades, actualizar cantidades, eliminar productos y mostrar el inventario. El inventario se maneja con variables independientes para cada producto.
