2d9a375e742e7e40a59ffc36b19548bcb49a5e12
cristhianchimbo50 <chimbocristhian994@gmail.com>
2024-01-29 10:15:08 -0500

    se agrego el inventario como lista

--- Proyecto V1.py
+++ Proyecto V1.py
@@ -1,12 +1,6 @@
 # Inicio de Sesión
 print("Bienvenido Usuario")
 
-# inicializar de productos y cantidades
-Producto1, cantidadArroz = "Arroz", 0
-# Producto1 = "Arroz"
-# cantidadArroz = 0
-Producto2, cantidadCola = "Cola", 0
-Producto3, cantidadHelado = "Helado", 0
 intentos = 0
 
 while True:
@@ -21,15 +15,17 @@ while True:
 
         intentos = intentos + 1
 
-        if intentos == 3 :
-            print("Ha superado el maximo de intentos.\n")
+        if intentos == 3:
+            print("Ha superado el máximo de intentos.\n")
             raise SystemExit
 
+# Inventario
+inventario = {}
 
 while True:
-    print("\n\n----- Menú de Inventario -----")
+    print("\n----- Menú de Inventario -----")
     print("1. Agregar Producto")
-    print("2. Actualizar Existencias")
+    print("2. Actualizar Stock")
     print("3. Eliminar Producto")
     print("4. Mostrar Inventario")
     print("5. Salir")
@@ -37,63 +33,36 @@ while True:
     opcion = input("Seleccione una opción: ")
 
     if opcion == '1':
-        print("Agregar Producto...\n")
         nombre_producto = input("Ingrese el nombre del nuevo producto: ")
-        existencias = int(input("Ingrese las existencias iniciales: "))
-
-        #existencias es la cantidad que existe de X producto
-        if nombre_producto == "Arroz":
-            cantidadArroz = cantidadArroz + existencias
-        elif nombre_producto == "Cola":
-            cantidadArroz = cantidadArroz + existencias
-        elif nombre_producto == "Helado":
-            cantidadHelado = cantidadHelado + existencias
-        else:
-            print("Producto no válido. Inténtelo nuevamente.")
-
+        stock = int(input("Ingrese el Stock inicial: "))
+        inventario[nombre_producto] = stock
         print("Producto agregado con éxito.")
 
-
     elif opcion == '2':
-        print("Actualizar Existencia...\n")
         nombre_producto = input("Ingrese el nombre del producto a actualizar: ")
-        existencias = int(input("Ingrese las nuevas existencias: "))
-        
-        if nombre_producto == "Arroz":
-            cantidadArroz = existencias
-        elif nombre_producto == "Cola":
-            cantidadCola = existencias
-        elif nombre_producto == "Helado":
-            cantidadHelado = existencias
+        if nombre_producto in inventario:
+            stock = int(input("Ingrese las nuevas existencias: "))
+            inventario[nombre_producto] = stock
+            print("Stock actualizado con éxito.")
         else:
-            print("Producto no encontrado. Inténtelo nuevamente.")
-
-        print("Existencias actualizadas con éxito.")
+            print("Producto no encontrado en el inventario.")
 
     elif opcion == '3':
-        print("Eliminar Producto...\n")
         nombre_producto = input("Ingrese el nombre del producto a eliminar: ")
-        if nombre_producto == "Arroz":
-            cantidadArroz = 0
-        elif nombre_producto == "Cola":
-            cantidadCola = 0
-        elif nombre_producto == "Helado":
-            cantidadHelado = 0
+        if nombre_producto in inventario:
+            del inventario[nombre_producto]
+            print("Producto eliminado con éxito.")
         else:
-            print("Producto no encontrado. Inténtelo nuevamente.")
-
-        print("Producto eliminado con éxito.")
+            print("Producto no encontrado en el inventario.")
 
     elif opcion == '4':
         print("\nInventario:")
-        print(Producto1 + ": " + str(cantidadArroz) + " unidades")
-        print(Producto2 + ": " + str(cantidadCola) + " unidades")
-        print(Producto3 + ": " + str(cantidadHelado) + " unidades")
-
+        for producto, stock in inventario.items():
+            print(f"{producto}: {stock} unidades")
 
     elif opcion == '5':
         print("Cerrando el Sistema")
         raise SystemExit
 
     else:
-        print("Opción invalida, intentar nuevamente.")
+        print("Opción inválida, intentar nuevamente.")
