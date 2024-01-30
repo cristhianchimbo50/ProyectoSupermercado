f1ead9dee401658332b044d2cb557b05825a4aa1
cristhianchimbo50 <chimbocristhian994@gmail.com>
2024-01-30 16:31:53 -0500

    final

--- Proyecto V1.py
+++ Proyecto V1.py
@@ -1,68 +1,135 @@
-# Inicio de Sesión
-print("Bienvenido Usuario")
+# Inicialización de variables
+i = 1
+n = 10
+intent = 0
+ses = 0
+opc = 0
+vent = 0
+suel_tot = 0
+cant_pro = 0
+exis_leche = 0
+exis_atun = 0
+exis_huevos = 0
+exis_papel = 0
+exis_alcohol = 0
+exis_curitas = 0
+exis_gasas = 0
+exis_sal = 0
+exis_cafe = 0
+exis_arroz = 0
+suma = 0
+desc = 0
+precio = 0
+total_cancelar = 0
+usu = ""
+cont = ""
+nom_prod = ""
+resp = ""
 
-intentos = 0
+while intent < 3 and ses == 0:
+    usu = input('Ingrese el usuario: ')
+    cont = input('Ingrese la contraseña: ')
+    
+    if usu == 'tienda':
+        if cont == 'T2024':
+            print('**Bienvenidos al Sistema**')
+            ses = 1
+            print('**Bienvenidos al siguiente menú**')
+            print('1. Visualizar el total a cancelar')
+            print('2. Visualizar el bono de los empleados')
+            print('**Inventario del Supermercado**')
+            print('Bienvenidos al siguiente menú')
+            print('3. Agregar productos y muestra el inventario')
+            opc = int(input('Seleccione 1 de las 3 opciones: '))
+            
+            if opc == 1:
+                n = int(input('Ingrese cuántos productos compró: '))
+                while i <= n:
+                    precio = float(input(f'Ingrese el precio del producto {i}: '))
+                    suma = suma + precio
+                    i = i + 1
+                if suma < 50:
+                    total_cancelar = suma
+                elif 50 <= suma <= 99:
+                    desc = suma * 0.05
+                    total_cancelar = suma - desc
+                elif 100 <= suma <= 149:
+                    desc = suma * 0.10
+                    total_cancelar = suma - desc
+                elif 150 <= suma <= 199:
+                    desc = suma * 0.15
+                    total_cancelar = suma - desc
+                elif 200 <= suma <= 249:
+                    desc = suma * 0.20
+                    total_cancelar = suma - desc
+                print(f'El total a cancelar es: {total_cancelar}')
 
-while True:
-    usuario = input("Ingrese su nombre de usuario: ")
-    contrasena = input("Ingrese su contraseña: ")
+            elif opc == 2:
+                vent = float(input('Ingrese cuánto vendió semanalmente: '))
+                if vent >= 1000:
+                    suel_tot = 450 + 100
+                    print(f'Usted obtuvo un bono, su sueldo total es: {suel_tot}')
+                else:
+                    suel_tot = 450
+                    print(f'Usted no obtuvo bono, su sueldo total es: {suel_tot}')
 
-    if usuario == '1' and contrasena == '1':
-        print("Inicio de sesión exitoso.\n")
-        break
-    else:
-        print("\n\nNombre de usuario o contraseña incorrectos, intentar nuevamente.")
+            elif opc == 3:
+                i = 1
+                resp = input('¿Desea ingresar algún producto? (SI-NO): ')
+                if resp.upper() == "SI":
+                    n = int(input('¿Cuántas veces desea ingresar productos?: '))
+                    while i <= n:
+                        nom_prod = input('Ingrese uno de los siguientes productos (Leche, Atún, Huevos, Papel higiénico, Alcohol, Curitas, Gasas, Sal, Café, Arroz): ')
+                        cant_pro = int(input('Ingrese el número de existencias del producto: '))
+                        if nom_prod == "Leche":
+                            exis_leche += cant_pro
+                        elif nom_prod == "Atun":
+                            exis_atun += cant_pro
+                        elif nom_prod == "Huevos":
+                            exis_huevos += cant_pro
+                        elif nom_prod == "Papel":
+                            exis_papel += cant_pro
+                        elif nom_prod == "Alcohol":
+                            exis_alcohol += cant_pro
+                        elif nom_prod == "Curitas":
+                            exis_curitas += cant_pro
+                        elif nom_prod == "Gasas":
+                            exis_gasas += cant_pro
+                        elif nom_prod == "Sal":
+                            exis_sal += cant_pro
+                        elif nom_prod == "Cafe":
+                            exis_cafe += cant_pro
+                        elif nom_prod == "Arroz":
+                            exis_arroz += cant_pro
+                        else:
+                            print("Error al ingresar producto, inténtelo de nuevo")
+                        i += 1
+                    print("Productos ingresados con éxito")
+                else:
+                    print("Usted no ha querido ingresar algún producto")
+                    
+                print("El Inventario de productos es:")
+                print("Leche: ", exis_leche)
+                print("Atun: ", exis_atun)
+                print("Huevos: ", exis_huevos)
+                print("Papel Higiénico: ", exis_papel)
+                print("Alcohol: ", exis_alcohol)
+                print("Curitas: ", exis_curitas)
+                print("Gasas: ", exis_gasas)
+                print("Sal: ", exis_sal)
+                print("Cafe: ", exis_cafe)
+                print("Arroz: ", exis_arroz)
 
-        intentos = intentos + 1
+            else:
+                print("Opción no válida")
 
-        if intentos == 3:
-            print("Ha superado el máximo de intentos.\n")
-            raise SystemExit
-
-# Inventario
-inventario = {}
-
-while True:
-    print("\n----- Menú de Inventario -----")
-    print("1. Agregar Producto")
-    print("2. Actualizar Stock")
-    print("3. Eliminar Producto")
-    print("4. Mostrar Inventario")
-    print("5. Salir")
-
-    opcion = input("Seleccione una opción: ")
-
-    if opcion == '1':
-        nombre_producto = input("Ingrese el nombre del nuevo producto: ")
-        stock = int(input("Ingrese el Stock inicial: "))
-        inventario[nombre_producto] = stock
-        print("Producto agregado con éxito.")
-
-    elif opcion == '2':
-        nombre_producto = input("Ingrese el nombre del producto a actualizar: ")
-        if nombre_producto in inventario:
-            stock = int(input("Ingrese las nuevas existencias: "))
-            inventario[nombre_producto] = stock
-            print("Stock actualizado con éxito.")
         else:
-            print("Producto no encontrado en el inventario.")
-
-    elif opcion == '3':
-        nombre_producto = input("Ingrese el nombre del producto a eliminar: ")
-        if nombre_producto in inventario:
-            del inventario[nombre_producto]
-            print("Producto eliminado con éxito.")
-        else:
-            print("Producto no encontrado en el inventario.")
-
-    elif opcion == '4':
-        print("\nInventario:")
-        for producto, stock in inventario.items():
-            print(f"{producto}: {stock} unidades")
-
-    elif opcion == '5':
-        print("Cerrando el Sistema")
-        raise SystemExit
+            print('La contraseña es incorrecta')
+            intent += 1
 
     else:
-        print("Opción inválida, intentar nuevamente.")
+        print('El usuario es incorrecto')
+        intent += 1
+
+if intent == 3:
+    print('Usuario bloqueado, utilizó 3 intentos')
