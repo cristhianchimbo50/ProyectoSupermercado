fd0f320c61bcbe4584e57ed8b5cfbb71280d7355
cristhianchimbo50 <chimbocristhian994@gmail.com>
2024-01-30 22:57:03 -0500

    hola v1

--- Proyecto V1.py
+++ Proyecto V1.py
@@ -26,111 +26,119 @@ cont = ""
 nom_prod = ""
 resp = ""
 
-while intent < 3 and ses == 0:
-    usu = input('Ingrese el usuario: ')
-    cont = input('Ingrese la contraseña: ')
-    
-    if usu == 'tienda':
-        if cont == 'T2024':
-            print('**Bienvenidos al Sistema**')
-            ses = 1
-            print('**Bienvenidos al siguiente menú**')
-            print('1. Visualizar el total a cancelar')
-            print('2. Visualizar el bono de los empleados')
-            print('**Inventario del Supermercado**')
-            print('Bienvenidos al siguiente menú')
-            print('3. Agregar productos y muestra el inventario')
-            opc = int(input('Seleccione 1 de las 3 opciones: '))
-            
-            if opc == 1:
-                n = int(input('Ingrese cuántos productos compró: '))
-                while i <= n:
-                    precio = float(input(f'Ingrese el precio del producto {i}: '))
-                    suma = suma + precio
-                    i = i + 1
-                if suma < 50:
-                    total_cancelar = suma
-                elif 50 <= suma <= 99:
-                    desc = suma * 0.05
-                    total_cancelar = suma - desc
-                elif 100 <= suma <= 149:
-                    desc = suma * 0.10
-                    total_cancelar = suma - desc
-                elif 150 <= suma <= 199:
-                    desc = suma * 0.15
-                    total_cancelar = suma - desc
-                elif 200 <= suma <= 249:
-                    desc = suma * 0.20
-                    total_cancelar = suma - desc
-                print(f'El total a cancelar es: {total_cancelar}')
+        
+while True:
+    usuario = input("Ingrese su nombre de usuario: ")
+    contrasena = input("Ingrese su contraseña: ")
 
-            elif opc == 2:
-                vent = float(input('Ingrese cuánto vendió semanalmente: '))
-                if vent >= 1000:
-                    suel_tot = 450 + 100
-                    print(f'Usted obtuvo un bono, su sueldo total es: {suel_tot}')
-                else:
-                    suel_tot = 450
-                    print(f'Usted no obtuvo bono, su sueldo total es: {suel_tot}')
-
-            elif opc == 3:
-                i = 1
-                resp = input('¿Desea ingresar algún producto? (SI-NO): ')
-                if resp.upper() == "SI":
-                    n = int(input('¿Cuántas veces desea ingresar productos?: '))
-                    while i <= n:
-                        nom_prod = input('Ingrese uno de los siguientes productos (Leche, Atún, Huevos, Papel higiénico, Alcohol, Curitas, Gasas, Sal, Café, Arroz): ')
-                        cant_pro = int(input('Ingrese el número de existencias del producto: '))
-                        if nom_prod == "Leche":
-                            exis_leche = exis_leche + cant_pro
-                        elif nom_prod == "Atun":
-                            exis_atun = exis_atun + cant_pro
-                        elif nom_prod == "Huevos":
-                            exis_huevos = exis_huevos + cant_pro
-                        elif nom_prod == "Papel":
-                            exis_papel = exis_papel + cant_pro
-                        elif nom_prod == "Alcohol":
-                            exis_alcohol = exis_alcohol + cant_pro
-                        elif nom_prod == "Curitas":
-                            exis_curitas = exis_curitas + cant_pro
-                        elif nom_prod == "Gasas":
-                            exis_gasas = exis_gasas + cant_pro
-                        elif nom_prod == "Sal":
-                            exis_sal = exis_sal + cant_pro
-                        elif nom_prod == "Cafe":
-                            exis_cafe = exis_cafe + cant_pro
-                        elif nom_prod == "Arroz":
-                            exis_arroz = exis_arroz + cant_pro
-
-                        else:
-                            print("Error al ingresar producto, inténtelo de nuevo")
-                        i = i + 1
-                    print("Productos ingresados con éxito")
-                else:
-                    print("Usted no ha querido ingresar algún producto")
-                    
-                print("El Inventario de productos es:")
-                print("Leche: ", exis_leche)
-                print("Atun: ", exis_atun)
-                print("Huevos: ", exis_huevos)
-                print("Papel Higiénico: ", exis_papel)
-                print("Alcohol: ", exis_alcohol)
-                print("Curitas: ", exis_curitas)
-                print("Gasas: ", exis_gasas)
-                print("Sal: ", exis_sal)
-                print("Cafe: ", exis_cafe)
-                print("Arroz: ", exis_arroz)
-
-            else:
-                print("Opción no válida")
-
-        else:
-            print('La contraseña es incorrecta')
-            intent += 1
-
+    if usuario == 'tienda' and contrasena == 'T2024':
+        print("Inicio de sesión exitoso.\n")
+        break
     else:
-        print('El usuario es incorrecto')
-        intent += 1
+        print("\nEl usuario o contraseña es incorrecto\n")
+        intent = intent + 1
+        
+        if intent == 3:
+            print("\nUsuario bloqueado, utilizó 3 intentos\n")
+            raise SystemExit
 
-if intent == 3:
-    print('Usuario bloqueado, utilizó 3 intentos')
+while True:
+    print("**Bienvenidos al Sistema**")
+    ses = 1
+    print("\n**Bienvenidos al siguiente menú**\n")
+    print("1. Visualizar el total a cancelar")
+    print("2. Visualizar el bono de los empleados")
+    print("\n**Inventario del Supermercado**\n")
+    print("\nBienvenidos al siguiente menú\n")
+    print("3. Agregar productos y muestra el inventario")
+    print("4. Salir del Sistema")
+    opc = int(input("Seleccione 1 de las 3 opciones: "))
+        
+    if opc == 1:
+        n = int(input("Ingrese cuántos productos compró: "))
+        while i <= n:
+            precio = float(input("Ingrese el precio del producto " , i , ": "))
+            suma = suma + precio
+            i = i + 1
+            if suma < 50:
+                total_cancelar = suma
+            elif 50 <= suma <= 99:
+                desc = suma * 0.05
+                total_cancelar = suma - desc
+            elif 100 <= suma <= 149:
+                desc = suma * 0.10
+                total_cancelar = suma - desc
+            elif 150 <= suma <= 199:
+                desc = suma * 0.15
+                total_cancelar = suma - desc
+            elif 200 <= suma <= 249:
+                desc = suma * 0.20
+                total_cancelar = suma - desc
+                
+            print("El total a cancelar es:", total_cancelar)
+
+    elif opc == 2:
+        vent = float(input("Ingrese cuánto vendió semanalmente: "))
+        if vent >= 1000:
+            suel_tot = 450 + 100
+            print("Usted obtuvo un bono, su sueldo total es: ", suel_tot)
+        else:
+            suel_tot = 450
+            print("Usted no obtuvo bono, su sueldo total es: ", suel_tot)
+
+    elif opc == 3:
+        i = 1
+        resp = input("¿Desea ingresar algún producto? (SI-NO): ")
+        if resp.upper() == "SI":
+            n = int(input("¿Cuántas veces desea ingresar productos?: "))
+            while i <= n:
+                nom_prod = input("Ingrese uno de los siguientes productos (Leche, Atún, Huevos, Papel higiénico, Alcohol, Curitas, Gasas, Sal, Café, Arroz): ")
+                cant_pro = int(input("Ingrese el número de existencias del producto: "))
+                if nom_prod.upper() == "LECHE":
+                    exis_leche = exis_leche + cant_pro
+                elif nom_prod.upper() == "ATUN":
+                    exis_atun = exis_atun + cant_pro
+                elif nom_prod.upper() == "HUEVOS":
+                    exis_huevos = exis_huevos + cant_pro
+                elif nom_prod.upper() == "PAPEL":
+                    exis_papel = exis_papel + cant_pro
+                elif nom_prod.upper() == "ALCOHOL":
+                    exis_alcohol = exis_alcohol + cant_pro
+                elif nom_prod.upper() == "CURITAS":
+                    exis_curitas = exis_curitas + cant_pro
+                elif nom_prod.upper() == "GASAS":
+                    exis_gasas = exis_gasas + cant_pro
+                elif nom_prod.upper() == "SAL":
+                    exis_sal = exis_sal + cant_pro
+                elif nom_prod.upper() == "CAFE":
+                    exis_cafe = exis_cafe + cant_pro
+                elif nom_prod.upper() == "ARROZ":
+                    exis_arroz = exis_arroz + cant_pro
+                else:
+                    print("Error al ingresar producto, inténtelo de nuevo")
+                i = i + 1
+                print("Productos ingresados con éxito")
+            else:
+                print("Usted no ha querido ingresar algún producto")
+                
+            print("\nEl Inventario de productos es:\n")
+            print("Leche: ", exis_leche)
+            print("Atun: ", exis_atun)
+            print("Huevos: ", exis_huevos)
+            print("Papel Higiénico: ", exis_papel)
+            print("Alcohol: ", exis_alcohol)
+            print("Curitas: ", exis_curitas)
+            print("Gasas: ", exis_gasas)
+            print("Sal: ", exis_sal)
+            print("Cafe: ", exis_cafe)
+            print("Arroz: ", exis_arroz)
+        else:
+            print("Opción no válida")
+    elif opc == 4:
+        print("SALIENDO DEL SISTEMA")
+        raise SystemExit
+    else:
+        print("Opción no válida")
+        raise SystemExit
+
+
